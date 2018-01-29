7717083d9f8583cf06657061a48cc60d7945801b
blakeharris <harrisblake85@gmail.com>
2018-01-29 12:39:55 -0700

    Resolve Homepage Typo

--- src/Home.js
+++ src/Home.js
@@ -45,7 +45,7 @@ class Home extends Component {
         <h2>Creatives For A Cause</h2>
         <p>Creatives For A Cause allows creative people to help many causes through <em>Democracy!</em> The top Submission from our creatives is chosen at the end of every month to go into production! </p>
         <p>When purchasing our past productions or going into pre-ordering our winner you get to select the cause you support!</p>
-        <p>Sign-Up to be a creative today! All submissions are purchases are welcome!</p>
+        <p>Sign-Up to be a creative today! All submissions and purchases are welcome!</p>
 
       </div>
     )
